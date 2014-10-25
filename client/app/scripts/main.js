6935b6c07a29c87d5dff983995c7a981f66b7c50
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2014-10-25 16:44:47 +0200

    use fance frontend with fancy backend :)

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -1,7 +1,27 @@
-$.ready(function() {
-  console.log('helloo');
+$(document).ready(function () {
+    console.log('helloo');
+
+    $('#input').keyup(function (e) {
+        $('#character-count').html(e.target.value.length);
+    });
+    var socket = io();
+    $('#input').keyup(function (e) {
+        if(e.keyCode == 13) {
+            console.log('tweet: ' + $("#input").val());
+            socket.emit('tweet', $('#input').val());
+        }
+    });
+
+    socket.on('tweet analysis', function (data) {
+        console.log('we received analysis: ' + data);
+        var result = JSON.parse(data);
+        for (var hashtag in result) {
+            if (result.hasOwnProperty(hashtag)) {
+                $('body').append('<br>');
+                $('body').append(hashtag + '<br>');
+                $('body').append('trenddata:' + result.burda.trend.toString() + '<br>');
+            }
+        }
+    });
 });
 
-$('#input').keyup(function(e){
-  $('#character-count').html(e.target.value.length);
-});
