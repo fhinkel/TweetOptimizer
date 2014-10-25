8b565a0cd0c8b02504cb5a27ce77cd20e38f4957
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2014-10-25 16:12:36 +0200

    typos

--- tests/interfaceToPython.js
+++ tests/interfaceToPython.js
@@ -3,7 +3,7 @@ var getAnalysis= require('../src/interfaceToPython').getAnalysis;
 
 describe('tweet', function () {
 
-    it('should return 200', function (next) {
+    it('should return hello World as JSON', function (next) {
         getAnalysis("tweet", function(error, response) {
            response.should.equal('{"hello": "Hello world!"}');
            next()
