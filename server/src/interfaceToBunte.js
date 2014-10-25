6048e589f4702c8010dc3a6cf35156ea226f9b73
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2014-10-25 19:30:02 +0200

    fix test

--- server/src/interfaceToBunte.js
+++ server/src/interfaceToBunte.js
@@ -22,7 +22,7 @@ exports.getAnalysis = function(next) {
 
         res.on('end', function () {
             next(null, JSON.parse(data));
-            console.log(data);
+            //console.log(data);
         });
 
     }).on('error', function (e) {
