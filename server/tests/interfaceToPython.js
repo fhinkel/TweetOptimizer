a657efa98cbb4d152ce986def74d3d263c0eb0eb
dotch <christian.x.weiss@gmail.com>
2014-10-26 00:27:19 +0200

    some backend (hacky) fixes and updated frontend. tests not fixed yet

--- server/tests/interfaceToPython.js
+++ server/tests/interfaceToPython.js
@@ -7,15 +7,14 @@ describe('tweet', function () {
 
     it('should return the first 3 users', function (next) {
         relatedUsers("#burda", function (error, response) {
-            response.toString()
-                .should.be.equal("@princesandthepe,@ibmbigdata,");
+            response[0].tag.should.be.equal('@simplymrt');
             next()
         });
     });
 
     it('should return the first 3 related hash tags', function (next) {
         relatedTags("#burda", function (error, response) {
-            response.toString()
+            JSON.stringify(response[1])
                 .should.be.equal("#patterns,#sewing,#antique");
             next()
         });
